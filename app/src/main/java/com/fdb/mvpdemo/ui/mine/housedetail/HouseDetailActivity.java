0c0a418636b1d7fe007008ed448e03ce4302e74d
xieguofeng <2118117466@qq.com>
2020-04-19 17:51:53 +0800

    #

--- app/src/main/java/com/fdb/mvpdemo/ui/mine/housedetail/HouseDetailActivity.java
+++ app/src/main/java/com/fdb/mvpdemo/ui/mine/housedetail/HouseDetailActivity.java
@@ -2,7 +2,6 @@ package com.fdb.mvpdemo.ui.mine.housedetail;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,11 +16,12 @@ import com.fdb.mvpdemo.widget.statusview.StatusView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class HouseDetailActivity extends BaseActivity<HouseDetailContract.Presenter> implements HouseDetailContract.View {
+public class HouseDetailActivity extends BaseActivity<HouseDetailContract.Presenter> implements HouseDetailContract.View, ContentLayout.OnConetntListener {
     @BindView(R.id.tv_content)
     TextView mTvContent;
     private String mId;
     private StatusView mStatusView;
+    private ContentLayout mContentLayout;
 
     @Override
     protected HouseDetailContract.Presenter createPresenter() {
@@ -30,24 +30,9 @@ public class HouseDetailActivity extends BaseActivity<HouseDetailContract.Presen
 
     @Override
     protected void initialize() {
-        setContentView(new ContentLayout(getActivity()) {
-            @Override
-            public void loadData(BaseNetCallback<DemandDetail> netCallback) {
-                getPresenter().getDetail("961", netCallback);
-            }
-
-            @Override
-            public View getConetntView() {
-                View view = ViewUtils.inflate(getActivity(), R.layout.activity_house_detail);
-                ButterKnife.bind(getActivity(), view);
-                return view;
-            }
-
-            @Override
-            public void showContent(DemandDetail bean) {
-                mTvContent.setText(bean.Data.toString());
-            }
-        });
+        mContentLayout = new ContentLayout(getActivity());
+        mContentLayout.setOnConetntListener(this);
+        setContentView(mContentLayout);
 
         getIntentData();
     }
@@ -64,9 +49,25 @@ public class HouseDetailActivity extends BaseActivity<HouseDetailContract.Presen
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // TODO: add setContentView(...) invocation
-        ButterKnife.bind(this);
+    public void loadData(BaseNetCallback<DemandDetail> netCallback) {
+        getPresenter().getDetail("961", netCallback);
+    }
+
+    @Override
+    public View getConetntView() {
+        View view = ViewUtils.inflate(getActivity(), R.layout.activity_house_detail);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void showContent(final DemandDetail bean) {
+        mTvContent.setText(bean.Data.toString());
+        mTvContent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mContentLayout.refreshData();
+            }
+        });
     }
 }
--- app/src/main/java/com/fdb/mvpdemo/widget/ContentLayout.java
+++ app/src/main/java/com/fdb/mvpdemo/widget/ContentLayout.java
@@ -8,6 +8,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import com.fdb.baselibrary.base.BaseApplication;
 import com.fdb.baselibrary.bean.DataErrorBean;
 import com.fdb.baselibrary.network.callback.BaseNetCallback;
 import com.fdb.mvpdemo.R;
@@ -24,12 +25,12 @@ import rx.Subscription;
  * Author dontlo
  * Date   2020/4/18.
  */
-public abstract class ContentLayout extends FrameLayout {
+public class ContentLayout extends FrameLayout {
     @BindView(R.id.srl_refresh)
     SwipeRefreshLayout mSrlRefresh;
     @BindView(R.id.status_view)
     StatusView mStatusView;
-    private Context mContext;
+    private Context mContext = BaseApplication.getApp();
 
     public ContentLayout(@NonNull Context context) {
         this(context, null);
@@ -37,36 +38,47 @@ public abstract class ContentLayout extends FrameLayout {
 
     public ContentLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init(context);
     }
 
-    private void init(Context context) {
-        mContext = context;
-
+    private void init() {
         View view = View.inflate(mContext, R.layout.view_layout_content, this);
         ButterKnife.bind(this, view);
 
+        //状态页配置
         mStatusView.config(new StatusViewBuilder.Builder()
                 .setOnErrorRetryClickListener(new OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         mStatusView.showLoadingView();
-                        loadData(mNetCallback);
+                        if (mOnConetntListener != null) {
+                            mOnConetntListener.loadData(mNetCallback);
+                        }
                     }
                 }).build());
 
+        //刷新回调
         mSrlRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                loadData(mNetCallback);
+                if (mOnConetntListener != null) {
+                    mOnConetntListener.loadData(mNetCallback);
+                }
             }
         });
 
-        mSrlRefresh.addView(getConetntView());
+        //添加内容布局
+        if (mOnConetntListener != null) {
+            mSrlRefresh.addView(mOnConetntListener.getConetntView());
+        }
 
-        loadData(mNetCallback);
+        if (mOnConetntListener != null) {
+            mOnConetntListener.loadData(mNetCallback);//加载数据
+        }
     }
 
+    /**
+     * 网络监听者
+     */
     BaseNetCallback<DemandDetail> mNetCallback = new BaseNetCallback<DemandDetail>() {
         @Override
         public void onPrepare(Subscription subscription) {
@@ -97,21 +109,61 @@ public abstract class ContentLayout extends FrameLayout {
                 mSrlRefresh.setRefreshing(false);
             }
             mStatusView.showContentView();
-            showContent(data);
+            if (mOnConetntListener != null) {
+                mOnConetntListener.showContent(data);
+            }
         }
     };
 
-    public abstract void loadData(BaseNetCallback<DemandDetail> netCallback);
+    /**
+     * 刷新数据
+     */
+    public void refreshData() {
+        mSrlRefresh.setRefreshing(true);
+        if (mOnConetntListener != null) {
+            mOnConetntListener.loadData(mNetCallback);
+        }
+    }
 
-    public abstract View getConetntView();
-
-    public abstract void showContent(DemandDetail bean);
-
+    /**
+     * 获取网络监听者
+     *
+     * @return
+     */
     public BaseNetCallback<DemandDetail> getNetCallback() {
         return mNetCallback;
     }
 
-    //    public abstract int getContentLayout(@LayoutRes int id);
+    private OnConetntListener mOnConetntListener;
+
+    public void setOnConetntListener(OnConetntListener listener) {
+        mOnConetntListener = listener;
+        init();
+    }
+
+    public interface OnConetntListener {
+        /**
+         * 加载数据
+         *
+         * @param netCallback
+         */
+        public abstract void loadData(BaseNetCallback<DemandDetail> netCallback);
+
+        /**
+         * 加载内容布局
+         *
+         * @return
+         */
+        public abstract View getConetntView();
+
+        /**
+         * 显示数据
+         *
+         * @param bean
+         */
+        public abstract void showContent(DemandDetail bean);
+    }
+
 }
 
 
